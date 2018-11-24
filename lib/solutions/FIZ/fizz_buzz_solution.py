f10b08a629c54526a57a454a62b3d8e3612c582a
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-11-24 20:58:54 +0000

    Sat Nov 24 20:58:54 GMT 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,7 +1,8 @@
 # noinspection PyUnusedLocal
 import collections
-def samedig(number):	
- 	countOfWords = collections.Counter(str(number)
+
+def same_dig(number):	
+ 	countOfWords = collections.Counter(str(number))
  	if len(number) == [countOfWords[i] for i in countOfWords if countOfWords[i]>1]:
 		return True
 	else:
@@ -9,7 +10,7 @@ def samedig(number):
 
 
 def fizz_buzz(number):
-    if (number%3==0 or '3' in str(number)) and (number%5 == 0 or '5' in str(number) and (number>10 and samedig(number)==True)):
+    if (number%3==0 or '3' in str(number)) and (number%5 == 0 or '5' in str(number) and (number>10 and same_dig(number)==True)):
         return "fizz buzz deluxe"
     elif (number%3==0 or '3' in str(number)) and (number%5 == 0 or '5' in str(number))
         return "fizz buzz"
