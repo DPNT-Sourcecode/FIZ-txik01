08510f385c9034173475fdef1a0976d7cff1eefe
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-11-24 21:04:55 +0000

    Sat Nov 24 21:04:55 GMT 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -2,16 +2,19 @@
 import collections
 
 def same_dig(number):	
- 	countOfWords = collections.Counter(str(number))
- 	if len(number) == [countOfWords[i] for i in countOfWords if countOfWords[i]>1]:
+ 	countOfWords = collections.Counter(number)
+     result = [countOfWords[i] for i in countOfWords if countOfWords[i]>1]
+ 	if len(number) == result:
 		return True
 	else:
 		return False
 
 
 def fizz_buzz(number):
-    if (number%3==0 or '3' in str(number)) and (number%5 == 0 or '5' in str(number) and (number>10 and same_dig(number)==True)):
+    if (number%3==0 or '3' in str(number)) and (number%5 == 0 or '5' in str(number)) and (number>10 and same_dig(number)==True)):
         return "fizz buzz deluxe"
+    elif (number>10 and same_dig(number)==True):
+         return "deluxe"
     elif (number%3==0 or '3' in str(number)) and (number%5 == 0 or '5' in str(number))
         return "fizz buzz"
     elif number%5 == 0 or '5' in str(number):
