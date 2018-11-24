ab9facb3bf3428e50466e2a54fc8c8d6de02ff3b
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-11-24 14:37:54 +0000

    Sat Nov 24 14:37:54 GMT 2018

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,7 +3,6 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    return "Hello, " + str(friend_name) + "!"
+    return "Hello, {}!".format(friend_name) 
 
-req = hello("World")
-print(req)
+print(hello("World"))
