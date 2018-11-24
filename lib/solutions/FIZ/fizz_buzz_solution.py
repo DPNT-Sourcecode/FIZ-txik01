b084c987ba0b89f5c27d061641563fa82c192c71
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-11-24 21:07:54 +0000

    Sat Nov 24 21:07:54 GMT 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -3,15 +3,14 @@ import collections
 
 def same_dig(number):	
  	countOfWords = collections.Counter(number)
-     result = [countOfWords[i] for i in countOfWords if countOfWords[i]>1]
- 	if len(number) == result:
+ 	if len(number)==[countOfWords[i] for i in countOfWords if countOfWords[i]>1]:
 		return True
 	else:
 		return False
 
 
 def fizz_buzz(number):
-    if (number%3==0 or '3' in str(number)) and (number%5 == 0 or '5' in str(number)) and (number>10 and same_dig(number)==True)):
+    if (number%3==0 or '3' in str(number)) and (number%5 == 0 or '5' in str(number)) and (number>10):
         return "fizz buzz deluxe"
     elif (number>10 and same_dig(number)==True):
          return "deluxe"
