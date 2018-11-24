04b3ac6d59483df790c7f863d71d06c9ea6bb08f
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-11-24 22:31:54 +0000

    Sat Nov 24 22:31:54 GMT 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -16,13 +16,17 @@ def same_dig(number):
 def fizz_buzz(number):
     if (number%3==0 or '3' in str(number)) and (number%5 == 0 or '5' in str(number)) and (number>10 and same_dig(number)==True):
         return "fizz buzz deluxe"
-    elif (number>10 and same_dig(number)==True):
-         return "deluxe"
+    elif (number%3==0 or '3' in str(number)) and (number>10 and same_dig(number)==True):
+        return "fizz deluxe"
+    elif (number%5 == 0 or '5' in str(number)) and (number>10 and same_dig(number)==True):
+        return "buzz deluxe"
     elif (number%3==0 or '3' in str(number)) and (number%5 == 0 or '5' in str(number)):
         return "fizz buzz"
     elif (number%5 == 0 or '5' in str(number)):
         return "buzz"
     elif (number%3==0 or '3' in str(number)):
         return "fizz"
+    elif (number>10 and same_dig(number)==True):
+         return "deluxe"
     else:
         return number
