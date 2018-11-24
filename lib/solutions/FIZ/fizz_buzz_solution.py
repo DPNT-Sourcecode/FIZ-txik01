2d0ea444b3bdecb210401b0c7c444f9ca26f262a
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-11-24 15:16:55 +0000

    Sat Nov 24 15:16:55 GMT 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,10 +1,12 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
-    if number%3==0 and number%5 == 0:
+    if (number%3==0 or 3 in number) and (number%5 == 0 or 5 in number):
         return "fizz buzz"
-    elif number%5 == 0:
+    elif number%5 == 0 or 5 in number:
         return "buzz"
-    elif number%3==0:
+    elif number%3==0 or 3 in number:
         return "fizz"
     else:
         return number
+
+print(fizz_buzz(35))
