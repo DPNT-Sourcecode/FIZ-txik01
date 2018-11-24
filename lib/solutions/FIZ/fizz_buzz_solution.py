d78d47b9c7857e890e26c12b1ee284dfae5f678f
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-11-24 23:40:54 +0000

    Sat Nov 24 23:40:54 GMT 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,6 +1,10 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
-    if (number%3==0 and '3' in str(number)) and (number % 2 == 0):
+    if (number%3==0 and '3' in str(number)) or (number%5 == 0 and '5' in str(number)) and (number % 2 == 0):
+        return "fizz buzz deluxe"
+    elif (number%3==0 and '3' in str(number)) or (number%5 == 0 and '5' in str(number)) and (number % 2 != 0):
+        return "fizz buzz fake deluxe"
+    elif (number%3==0 and '3' in str(number)) and (number % 2 == 0):
         return "fizz deluxe"
     elif (number%3==0 and '3' in str(number)) and (number % 2 != 0):
         return "fizz fake deluxe"
@@ -8,10 +12,6 @@ def fizz_buzz(number):
         return "buzz deluxe"
     elif (number%5 == 0 and '5' in str(number)) and (number % 2 != 0):
         return "buzz fake deluxe"
-    elif (number%3==0 and '3' in str(number)) or (number%5 == 0 and '5' in str(number)) and (number % 2 == 0):
-        return "fizz buzz deluxe"
-    elif (number%3==0 and '3' in str(number)) or (number%5 == 0 and '5' in str(number)) and (number % 2 != 0):
-        return "fizz buzz fake deluxe"
     elif (number%3==0 or '3' in str(number)) and (number%5 == 0 or '5' in str(number)):
         return "fizz buzz"
     elif (number%5 == 0 or '5' in str(number)):
