90dac612f87086ba66810300b9aa2c1bfdc406b5
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-11-25 01:20:33 +0000

    Sun Nov 25 01:20:33 GMT 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,7 +1,7 @@
 def fizz_buzz(number):
-    if (number%3==0 or '3' in str(number)) and (number%5 == 0 or '5' in str(number)) and ((number%3==0 and '3' in str(number)) or (number%5 == 0 and '5' in str(number)) and number % 2 == 0):
+    if (number%3==0 or '3' in str(number)) and (number%5 == 0 or '5' in str(number)) and ((number%3==0 and '3' in str(number)) or (number%5 == 0 and '5' in str(number))) and number % 2 == 0:
         return "fizz buzz deluxe"
-    elif (number%3==0 or '3' in str(number)) and (number%5 == 0 or '5' in str(number)) and ((number%3==0 and '3' in str(number)) or (number%5 == 0 and '5' in str(number)) and number % 2 != 0):
+    elif (number%3==0 or '3' in str(number)) and (number%5 == 0 or '5' in str(number)) and ((number%3==0 and '3' in str(number)) or (number%5 == 0 and '5' in str(number))) and number % 2 != 0:
         return "fizz buzz fake deluxe"
     elif (number%3 == 0 or '3' in str(number)) and ((number%3==0 and '3' in str(number)) or (number%5 == 0 and '5' in str(number))) and number % 2 == 0:
         return "fizz deluxe"
