1871bedb393a7653f88d22d3d6d9617d8bb426a2
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-11-25 00:07:54 +0000

    Sun Nov 25 00:07:54 GMT 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,8 +1,9 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
-    if (number%3==0 or '3' in str(number)) and (number%5 == 0 or '5' in str(number)) and (number % 2 == 0):
+
+    if ((number%3==0 or '3' in str(number)) and (number%5 == 0 or '5' in str(number))) and (number % 2 == 0):
         return "fizz buzz deluxe"
-    elif (number%3==0 or '3' in str(number)) and (number%5 == 0 or '5' in str(number)) and (number % 2 != 0):
+    elif ((number%3==0 or '3' in str(number)) and (number%5 == 0 or '5' in str(number))) and (number % 2 != 0):
         return "fizz buzz fake deluxe"
     elif (number%3==0 and '3' in str(number)) and (number % 2 == 0):
         return "fizz deluxe"
