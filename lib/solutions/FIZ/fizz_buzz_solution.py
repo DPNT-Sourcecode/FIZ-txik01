2c75400c5f184f6b213b7613cb0004923fdc7a1c
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-11-25 00:34:55 +0000

    Sun Nov 25 00:34:55 GMT 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,17 +1,15 @@
-# noinspection PyUnusedLocal
 def fizz_buzz(number):
-
-    if ((number%3==0 or '3' in str(number)) and (number%5 == 0 or '5' in str(number))) and (number % 2 == 0):
+    if (number%3==0 or '3' in str(number)) and (number%5 == 0 or '5' in str(number)) and ((number%3==0 and '3' in str(number)) or (number%5 == 0 and '5' in str(number)) and number % 2 == 0):
         return "fizz buzz deluxe"
-    elif ((number%3==0 and '3' in str(number)) and (number%5 == 0 and '5' in str(number))) and (number % 2 != 0):
+    elif (number%3==0 or '3' in str(number)) and (number%5 == 0 or '5' in str(number)) and ((number%3==0 and '3' in str(number)) or (number%5 == 0 and '5' in str(number)) and number % 2 != 0):
         return "fizz buzz fake deluxe"
-    elif (number%3==0 and '3' in str(number)) and (number % 2 == 0):
+    elif (number%3==0 or '3' in str(number)) and ((number%3==0 and '3' in str(number)) or (number%5 == 0 and '5' in str(number)) and number % 2 == 0):
         return "fizz deluxe"
-    elif (number%3==0 and '3' in str(number)) and (number % 2 != 0):
+    elif (number%3==0 or '3' in str(number)) and ((number%3==0 and '3' in str(number)) or (number%5 == 0 and '5' in str(number)) and number % 2 != 0):
         return "fizz fake deluxe"
-    elif (number%5 == 0 and '5' in str(number)) and (number % 2 == 0):
+    elif (number%5 == 0 or '5' in str(number)) and ((number%3==0 and '3' in str(number)) or (number%5 == 0 and '5' in str(number)) and number % 2 == 0):
         return "buzz deluxe"
-    elif (number%5 == 0 and '5' in str(number)) and (number % 2 != 0):
+    elif (number%5 == 0 or '5' in str(number)) and ((number%3==0 and '3' in str(number)) or (number%5 == 0 and '5' in str(number)) and number % 2 != 0):
         return "buzz fake deluxe"
     elif (number%3==0 or '3' in str(number)) and (number%5 == 0 or '5' in str(number)):
         return "fizz buzz"
